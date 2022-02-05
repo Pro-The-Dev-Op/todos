95cbf4e3f4cd3ac4404fed4dde8667e19a55aa1f
Pro-The-Dev-Op <raghaov@gmail.com>
2022-02-05 20:03:15 +0530

    make it better

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 export default function Header(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
+      <div className="contain er-fluid">
         <Link className="navbar-brand" to="/">
           {props.title}
         </Link>
